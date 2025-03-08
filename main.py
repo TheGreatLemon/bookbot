51998d013582d48773b9e90d98a2ede6da8d8926
Joseph Brasher <josephdbrasher@gmail.com>
2025-03-08 01:01:51 -0600

    add funcs to get text or word counts

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(file_handler):
+    with open(file_handler) as f:
+        return f.read()
+    
+def get_word_count(file_handler):
+    text = ""
+    with open(file_handler)as f:
+        text = f.read()
+    words = text.split()
+    word_count = len(words)
+    return f'{word_count} words found in the document'
+    
+    
+def main():
+    print(get_word_count("books/frankenstein.txt"))
+    
+    
+main()
